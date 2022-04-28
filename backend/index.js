18352ca525bc060215118008aebf5c2300e3b5fd
Akash Raj Singh <ars.150697@gmail.com>
2022-04-28 07:27:43 +0530

    Modifying code to use promises instead of python scripts. Removed DB call since DB is not set up on my machine

--- backend/index.js
+++ backend/index.js
@@ -15,17 +15,17 @@ app.use(cors())
 app.use(express.json())
 
 // MongoDB connection
-mongoose
-  .connect('mongodb://localhost:27017/algoTrade', {
-    useUnifiedTopology: true,
-    useNewUrlParser: true,
-  })
-  .then(() => {
-    console.info('DB connected')
-  })
-  .catch((error) => {
-    console.error('DB connection error')
-  })
+// mongoose
+//   .connect('mongodb://localhost:27017/algoTrade', {
+//     useUnifiedTopology: true,
+//     useNewUrlParser: true,
+//   })
+//   .then(() => {
+//     console.info('DB connected')
+//   })
+//   .catch((error) => {
+//     console.error('DB connection error')
+//   })
 
 // Root
 app.get('/', (req, res) => {
